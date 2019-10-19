1bbb9df90679c24769f7afd9ba9025d942e7437e
clach04 <clach04@gmail.com>
2019-10-19 12:44:56 -0700

    Quick and dirty winner logic

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
 
+const debug=false;
 const rock='rock';
 const paper='paper';
 const scissor='scissor';
+const player1='player1';
+const player2='player2';
+const draw='draw';
+
+function winner(player1_move, player2_move) {
+    // Result is one of: 'draw', player1, player2
+    switch (player1_move) {
+        case rock:
+                switch (player2_move) {
+                    case rock: return draw;
+                          break;
+                    case paper: return player2;
+                          break;
+                    case scissor: return player1;
+                          break;
+                }
+                break;
+
+        case paper:
+                switch (player2_move) {
+                    case rock: return player1;
+                          break;
+                    case paper: return draw;
+                          break;
+                    case scissor: return player2;
+                          break;
+                }
+                break;
+        case scissor:
+                switch (player2_move) {
+                    case rock: return player2;
+                          break;
+                    case paper: return player1;
+                          break;
+                    case scissor: return draw;
+                          break;
+                }
+                break;
+    }
+}
 
 function generate_keys() {
     // Version 1 :-)
@@ -22,5 +63,14 @@ function generate_keys() {
     return result;
 }
 
-//result = generate_keys();
-//console.log(result);
+if (debug){
+    result = generate_keys();
+    console.log(result);
+    console.log(JSON.stringify(result, null, 4));
+
+    result = winner(rock, scissor);
+    result = winner(rock, scissor);
+    console.log(result);
+    result = winner(scissor, rock);
+    console.log(result);
+}
