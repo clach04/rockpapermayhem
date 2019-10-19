d2f8de4886f455b5d928854d5d185fdcb3a9d57f
clach04 <clach04@gmail.com>
2019-10-19 12:27:54 -0700

    use constants rather than strings, help avoid typos

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 
-generate_keys() {
+const rock='rock';
+const paper='paper';
+const scissor='scissor';
+
+function generate_keys() {
     // Version 1 :-)
     var result={
         'player1': {
-                'rock': ['b'],
-                'paper': ['n'],
-                'scissor': ['m']
+                rock: ['b'],
+                paper: ['n'],
+                scissor: ['m']
         },
         'player2': {
-                'rock': ['1'],
-                'paper': ['2'],
-                'scissor': ['3']
+                rock: ['1'],
+                paper: ['2'],
+                scissor: ['3']
         }
     };
 
     //console.log(result);
     return result;
 }
+
+//result = generate_keys();
+//console.log(result);
