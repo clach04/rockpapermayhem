5e29ad8da3083d96bfc11138eea4bf59fe4daf4d
clach04 <clach04@gmail.com>
2019-10-19 12:21:46 -0700

    Dumb "random" key generator for initial prototype
    NOTE this needs to be moved around!

---
+++ main.js
@@ -0,0 +1,19 @@
+
+generate_keys() {
+    // Version 1 :-)
+    var result={
+        'player1': {
+                'rock': ['b'],
+                'paper': ['n'],
+                'scissor': ['m']
+        },
+        'player2': {
+                'rock': ['1'],
+                'paper': ['2'],
+                'scissor': ['3']
+        }
+    };
+
+    //console.log(result);
+    return result;
+}
